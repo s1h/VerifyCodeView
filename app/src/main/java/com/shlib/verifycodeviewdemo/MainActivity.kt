2e1a0ddd4cab1c18fb6f392b2ca4f804fed4f55b
hang.sun <sunh@eetrust.com>
2018-04-25 17:00:11 +0800

    生成依赖

--- app/src/main/java/com/shlib/verifycodeviewdemo/MainActivity.kt
+++ app/src/main/java/com/shlib/verifycodeviewdemo/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        verifyCodeView.setCompleteListener(object : VerifyCodeCompleteListener{
+        verifyCodeView.setCompleteListener(object : VerifyCodeCompleteListener {
             override fun verifyCodeComplete() {
                 Toast.makeText(this@MainActivity, verifyCodeView.getText(), Toast.LENGTH_SHORT).show()
             }
